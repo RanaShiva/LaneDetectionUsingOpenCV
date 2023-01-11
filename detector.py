ae1646bfa4fa983b7f3f1a9a3bc027d956c8b18f
RanaShiva <shivamrana.4wrk@gmail.com>
2023-01-11 15:56:02 +0530

    Lane detection using OpenCV Project started

---
+++ detector.py
@@ -0,0 +1,19 @@
+from matplotlib import pyplot as plt
+import cv2
+import numpy as np
+
+image = cv2.imread('road.png')
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+print(image.shape)
+height = image.shape[0]
+width = image.shape[1]
+
+region_of_interest_vertices = [
+    (0, height),
+    (width/2, height/2),
+
+]
+
+plt.imshow(image)
+plt.show()
