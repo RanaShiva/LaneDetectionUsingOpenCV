f3ea3588301eb352412dd476559cf539eb309224
RanaShiva <shivamrana.4wrk@gmail.com>
2023-01-11 16:56:55 +0530

    Changed the code for video files and added some video files

--- detector.py
+++ detector.py
@@ -17,38 +17,50 @@ def draw_the_lines(img, lines):
 
     for line in lines:
         for x1, y1, x2, y2 in line:
-            cv2.line(blank_image, (x1,y1), (x2,y2), (255, 0, 0), thickness = 5)
+            cv2.line(blank_image, (x1,y1), (x2,y2), (0, 255, 0), thickness = 5)
 
     img = cv2.addWeighted(img, 0.8, blank_image, 1, 0.0)
     return img
 
-image = cv2.imread('road.png')
-image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+# image = cv2.imread('road.png')
+# image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-print(image.shape)
-height = image.shape[0]
-width = image.shape[1]
+def process(image):
+    print(image.shape)
+    height = image.shape[0]
+    width = image.shape[1]
 
-region_of_interest_vertices = [
-    (0, height),
-    (width/2, height/2),
-    (width, height)
-]
-gray_image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-canny_image = cv2.Canny(gray_image, 100, 200)
-cropped_image = region_of_interest(canny_image,
-                                   np.array([region_of_interest_vertices], np.int32))
+    region_of_interest_vertices = [
+        (0, height),
+        (width/2, height/2),
+        (width, height)
+    ]
+    gray_image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    canny_image = cv2.Canny(gray_image, 100, 120)
+    cropped_image = region_of_interest(canny_image,
+                                       np.array([region_of_interest_vertices], np.int32))
 
-lines = cv2.HoughLinesP(cropped_image,
-                        rho = 6,
-                        theta = np.pi/60,
-                        threshold = 160,
-                        lines = np.array([]),
-                        minLineLength=40,
-                        maxLineGap=25)
+    lines = cv2.HoughLinesP(cropped_image,
+                            rho = 2,
+                            theta = np.pi/180,
+                            threshold = 50,
+                            lines = np.array([]),
+                            minLineLength=40,
+                            maxLineGap=100)
 
 
-image_with_lines = draw_the_lines(image, lines)
+    image_with_lines = draw_the_lines(image, lines)
+    return image_with_lines
 
-plt.imshow(image_with_lines)
-plt.show()
+cap = cv2.VideoCapture('road4.mp4')
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    frame = process(frame)
+    cv2.imshow("frame", frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+# plt.imshow(image_with_lines)
+# plt.show()
